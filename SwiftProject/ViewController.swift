e1205e11de86b9173009a294dd2d622cf592c7a7
Ray <waley518@gmail.com>
2022-06-20 15:57:18 +0800

    Initial Commit

---
+++ SwiftProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftProject
+//
+//  Created by Waley on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
